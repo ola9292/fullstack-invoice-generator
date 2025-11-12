8a961e1de0c5ec9b9f88096ded7a4c447f52ae61
admin <admin@admins-MBP.lan>
2025-11-12 21:22:21 +0000

    initial commit

--- resources/js/Pages/Invoice/Index.vue
+++ resources/js/Pages/Invoice/Index.vue
@@ -25,9 +25,9 @@ const props = defineProps({ invoices: Array })
                         <h5 class="card-title">From: {{ invoice.from_name }} - To: {{ invoice.to_name }}</h5>
                         <p class="card-text">{{ invoice.from_email }}</p>
                         <div>
-                            <a :href="`invoice/${invoice.invoice_number}`" class="btn btn-primary me-2"><i class="fa-solid fa-eye"></i> Preview</a>
-                            <Link :href="`invoice/${invoice.id}/delete`" class="btn btn-danger me-2" method="POST"><i class="fa-solid fa-trash-can"></i> Delete</Link>
-                            <Link :href="`send-invoice/${invoice.invoice_number}`" class="btn btn-success"><i class="fa-solid fa-inbox"></i> Send Invoice</Link>
+                            <a :href="`invoice/${invoice.invoice_number}`" class="btn btn-primary me-2 mb-2"><i class="fa-solid fa-eye"></i> Preview</a>
+                            <Link :href="`invoice/${invoice.id}/delete`" class="btn btn-danger me-2 mb-2" method="POST"><i class="fa-solid fa-trash-can"></i> Delete</Link>
+                            <Link :href="`send-invoice/${invoice.invoice_number}`" class="btn btn-success mb-2"><i class="fa-solid fa-inbox"></i> Send Invoice</Link>
                         </div>
 
                     </div>
